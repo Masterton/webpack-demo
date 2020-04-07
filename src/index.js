4fbdfa144c8a4b87a31f19b20c7b3b9d0199c170
Masterton <zhengcloud@foxmail.com>
2020-04-07 20:56:36 +0800

    创建一个 bundle 文件

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 function component() {
     var element = document.createElement('div');
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    // lodash 是由当前 script 脚本 import 导入进来的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
