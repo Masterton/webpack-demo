383876ea2f356fd34aa9694e6296b19c99eaf35f
Masterton <zhengcloud@foxmail.com>
2020-04-07 21:02:09 +0800

    加载 CSS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     var element = document.createElement('div');
 
     // lodash 是由当前 script 脚本 import 导入进来的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
 }
