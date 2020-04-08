48bc7d6a4e0fe6c6e30ca20dff72c281df6e3d12
Masterton <zhengcloud@foxmail.com>
2020-04-08 17:22:02 +0800

    指定环境

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { cube } from './math.js';
 // import printMe from './print.js';
 // import './style.css';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 function component() {
     // var element = document.createElement('div');
     // var btn = document.createElement('button');
