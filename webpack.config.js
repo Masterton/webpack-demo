b40d46e0b99fe347841bbd4a7a4ff7dfc5790faa
Masterton <zhengcloud@foxmail.com>
2020-04-08 18:21:18 +0800

    防止重复

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         index: './src/index.js',
         another: './src/another-module.js'
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Code Splitting'
-        })
-    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     }
 };
