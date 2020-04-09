3857e312700de3ad3cc22df680b124527e8dc171
Masterton <zhengcloud@foxmail.com>
2020-04-09 11:57:51 +0800

    添加 Workbox

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -11,11 +12,17 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HTMLWebpackPlugin({
-            title: 'shimming'
+            title: 'Progressive Web Application'
         }),
         new webpack.HashedModuleIdsPlugin(),
         new webpack.ProvidePlugin({
             join: ['lodash', 'join']
+        }),
+        new WorkboxPlugin.GenerateSW({
+            // 这些选项帮助 ServiceWorkers 快速启用
+            // 不允许遗留任何“旧的” ServiceWorkers
+            clientsClaim: true,
+            skipWaiting: true
         })
     ],
     output: {
